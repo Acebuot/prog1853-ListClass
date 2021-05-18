b565d517391381247c110b1a05f237479317d0dd
unknown <L7Buot@gmail.com>
2021-05-18 08:32:06 -0400

    first commit

---
+++ InClassList/FrmListCollection.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace InClassList
+{
+    partial class FrmListCollection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIndex = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnRemoveAt = new System.Windows.Forms.Button();
+            this.btnIndexOf = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(87, 15);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(100, 20);
+            this.txtValue.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 79);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 28);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add Value";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Value:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Index:";
+            // 
+            // txtIndex
+            // 
+            this.txtIndex.Location = new System.Drawing.Point(87, 45);
+            this.txtIndex.Name = "txtIndex";
+            this.txtIndex.Size = new System.Drawing.Size(100, 20);
+            this.txtIndex.TabIndex = 4;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(118, 79);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(101, 28);
+            this.btnInsert.TabIndex = 5;
+            this.btnInsert.Text = "Insert Value";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.BtnInsert_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(12, 113);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(100, 28);
+            this.btnRemove.TabIndex = 6;
+            this.btnRemove.Text = "Remove Value";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.BtnRemove_Click);
+            // 
+            // btnRemoveAt
+            // 
+            this.btnRemoveAt.Location = new System.Drawing.Point(118, 113);
+            this.btnRemoveAt.Name = "btnRemoveAt";
+            this.btnRemoveAt.Size = new System.Drawing.Size(100, 28);
+            this.btnRemoveAt.TabIndex = 7;
+            this.btnRemoveAt.Text = "Remove Value At";
+            this.btnRemoveAt.UseVisualStyleBackColor = true;
+            this.btnRemoveAt.Click += new System.EventHandler(this.BtnRemoveAt_Click);
+            // 
+            // btnIndexOf
+            // 
+            this.btnIndexOf.Location = new System.Drawing.Point(12, 147);
+            this.btnIndexOf.Name = "btnIndexOf";
+            this.btnIndexOf.Size = new System.Drawing.Size(100, 28);
+            this.btnIndexOf.TabIndex = 8;
+            this.btnIndexOf.Text = "Index Of Value";
+            this.btnIndexOf.UseVisualStyleBackColor = true;
+            this.btnIndexOf.Click += new System.EventHandler(this.BtnIndexOf_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(118, 147);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 28);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear Values";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(12, 181);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(206, 28);
+            this.btnDisplay.TabIndex = 10;
+            this.btnDisplay.Text = "Display Values";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.BtnDisplay_Click);
+            // 
+            // FrmListCollection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 222);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnIndexOf);
+            this.Controls.Add(this.btnRemoveAt);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtIndex);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtValue);
+            this.Name = "FrmListCollection";
+            this.Text = "List<T> Collection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIndex;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnRemoveAt;
+        private System.Windows.Forms.Button btnIndexOf;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDisplay;
+    }
+}
+
---
+++ InClassList/FrmListCollection.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InClassList
+{
+    public partial class FrmListCollection : Form
+    {
+        public FrmListCollection()
+        {
+            InitializeComponent();
+        }
+
+        List<string> values = new List<string>();
+
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtValue.Text == "")
+            {
+                MessageBox.Show("ur pp smol", "kys psa");
+            }
+
+            else
+            {
+                // values.Add(input); = txtValue.Text;
+                string input = txtValue.Text;
+                values.Add(input);
+
+                //txtValue.Text = "";
+                txtValue.Clear();
+
+                MessageBox.Show(input + " added to index " + (values.Count - 1), "indexes start at 0");
+                txtValue.Focus();
+            }
+           
+        }
+
+        private void BtnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //values[int.Parse(txtIndex.Text)] = txtValue.Text;
+                string inputStr = txtValue.Text;
+                int inputInd = int.Parse(txtIndex.Text);
+                values.Insert(inputInd, inputStr);
+
+                // txtValue.Text = "";
+                // txtIndex.Text = "";
+                txtValue.Clear();
+                txtIndex.Clear();
+
+                //unnecessary bs
+                MessageBox.Show("Value " + values[inputInd] + " has been added to index " + inputInd, "indexes start at 0");
+                txtValue.Focus();
+            }
+
+            catch
+            {
+                MessageBox.Show("Please input valid value and index");
+                txtValue.Focus();
+            }
+        }
+
+        private void BtnDisplay_Click(object sender, EventArgs e)
+        {
+            StringBuilder damnSexy = new StringBuilder();
+            for(int i = 0; i < values.Count; i++)
+            {
+                damnSexy.Append("[" + i + "] " + values[i] + " ");
+            }
+            MessageBox.Show(damnSexy.ToString());
+
+            //foreach (string temp in values)
+            //{
+            //    damnSexy.Append(temp + "\n");
+            //}
+            //MessageBox.Show(damnSexy.ToString());
+
+            txtValue.Focus();
+        }
+
+        private void BtnRemove_Click(object sender, EventArgs e)
+        {
+            string input = txtValue.Text;
+            if (values.Remove(input))
+            {
+                MessageBox.Show("Value Removed", "Success", MessageBoxButtons.OK);
+                txtValue.Clear();
+                txtValue.Focus();
+            }
+            else
+            {
+                MessageBox.Show("What is Oklahoma's state abbreviation?", "We Trippin", MessageBoxButtons.OK);
+            }
+
+            
+
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            values.Clear();
+            MessageBox.Show("values cleared");
+            txtValue.Focus();
+        }
+
+        private void BtnRemoveAt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            //values.RemoveAt(int.Parse(txtIndex.Text));
+            //messagebox
+            int index = int.Parse(txtIndex.Text);
+            MessageBox.Show("value of " + values[index] + " at index " + index + " will be removed", "indexes start at 0", MessageBoxButtons.OK);
+            values.RemoveAt(index);
+
+            txtIndex.Clear();
+
+            }
+
+            catch
+            {
+                MessageBox.Show("Error 404: Your brain not found", "Bless your Soul");
+            }
+        }
+
+        private void BtnIndexOf_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtIndex.Text = values.IndexOf(txtValue.Text).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("ok");
+            }
+            
+        }
+    }
+}
